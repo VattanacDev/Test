0d95c682d0cdb60734601290c78d50c7ebfcd320
vattnac <vattanacgeek98@gmail.com>
2017-07-09 20:02:49 -0700

    first game

---
+++ GAME/UFO Game/Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb2d;
+    private int count;
+    public Text CountText;
+    public Text WinText;
+	
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        setCountText();
+        WinText.text = "";
+	}
+    public void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVerticaol = Input.GetAxis("Vertical");
+        Vector2 movment = new Vector2(moveHorizontal,moveVerticaol);
+        rb2d.AddForce(movment*speed);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            setCountText();
+        } 
+       
+        
+    }
+
+    void setCountText()
+    {
+        CountText.text = "Count" + count.ToString();
+        if (count >=12)
+        {
+            WinText.text = "F*K U";
+        }
+    }
+}
